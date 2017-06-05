aa6d92bcf66291bdd464c49ab8a774e980dcc682
MED2LABVN\hung.nguyen <Hungnguyen@med2lab.com>
2017-06-04 23:11:11 -0400

    init code

---
+++ client/bpm/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { BpmPage } from './app.po';
+
+describe('bpm App', () => {
+  let page: BpmPage;
+
+  beforeEach(() => {
+    page = new BpmPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
