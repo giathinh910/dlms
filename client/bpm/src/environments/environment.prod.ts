238af630604124eb67f8d6c1e1f6dc75b11f5000
Thinh Bui <giathinh910@gmail.com>
2017-06-17 17:03:16 +0700

    update bpm client: add .htacess on build

--- client/bpm/src/environments/environment.prod.ts
+++ client/bpm/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://192.168.2.101:8000/api/learner',
-    chatSocketUrl: 'http://192.168.2.101:8000/ws/chat'
+    apiUrl: 'http://192.168.56.101:8000/api/learner',
+    chatSocketUrl: 'http://192.168.56.101:8000/ws/chat'
 };
